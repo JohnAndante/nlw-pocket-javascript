2eb6845cf59849b59ede8835fa3b96a8d5e50d69
JohnAndante <walker_contas@hotmail.com>
2024-09-11 00:34:04 -0300

    #01 - feat: criada rota para criação de objetivos

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,31 @@
 import fastify from 'fastify'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+app.post('/goals', {
+    schema: {
+        body: z.object({
+            title: z.string(),
+            desiredWeeklyFrequency: z.number().int().min(1).max(7),
+        }),
+    }
+}, async (request, response) => {
+    const { title, desiredWeeklyFrequency } = request.body;
+
+    const data = await createGoal({
+        title,
+        desiredWeeklyFrequency,
+    });
+
+    return response.status(200).send({
+        message: "Meta criada com sucesso!",
+        data
+    });
+});
+
 app
     .listen({
         port: 3333,
