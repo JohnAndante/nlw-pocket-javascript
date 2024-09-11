c5845a347d3f3c851df6176f513dbb91c7a9a9c1
JohnAndante <walker_contas@hotmail.com>
2024-09-11 00:37:20 -0300

    #01 - feat: adicionada rota para registro de conclusão de etapas de uma meta

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -3,8 +3,27 @@ import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fas
 import { createGoal } from '../functions/create-goal'
 import z from 'zod'
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.post('/completions', {
+    schema: {
+        body: z.object({
+            goalId: z.string(),
+        }),
+}, async (request, response) => {
+    const { goalId } = request.body;
+
+    const data = await createGoalCompletion({
+        goalId,
+    });
+
+    return response.status(200).send({
+        message: "Frequência registrada com sucesso!",
+        data
+    });
+});
 app.post('/goals', {
     schema: {
         body: z.object({
